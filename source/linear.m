7c96984c682d2c9d7e8b3a622909681c57353c62
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2016-05-28 22:28:33 -0500

    Next update to regularizeNd. I calculated the cell index. However, the cell index may not be of use.

--- source/linear.m
+++ source/linear.m
@@ -10,7 +10,8 @@ w = cat(3, 1-fraction, fraction);
 
 % subscript for the 3rd dimensions of w
 index3 = cell(nDimensions, 1);
-[index3{:}] = ind2sub(2*ones(1,nDimensions), (1:2^nDimensions)');
+nodeNumber = (1:2^nDimensions);
+[index3{:}] = ind2sub(2*ones(1,nDimensions), nodeNumber');
 index3 = [index3{:}];
 % subscript of 2nd dimension w
 index2 = repmat(1:nDimensions, 2^nDimensions, 1);
@@ -18,16 +19,22 @@ index2 = repmat(1:nDimensions, 2^nDimensions, 1);
 % dimensions of w. transpose for later use with the prod function so that
 % it operates along the columns.
 index23 = sub2ind([nDimensions, 2], index2, index3)';
+% compute the index offset
+indexOffset = index3'-1;
 
 % preallocate before looping
-weights = nan(size(x,1),2^nDimensions);
-index = weights;
+weight = nan(size(x,1),2^nDimensions);
+index = weight;
 
 for iPoint = 1:size(x,1)
     localDimensionalWeights = w(iPoint, :,:);
+    localLowerIndex = lowerIndex(iPoint,:);
     % compute the weight for each point by computing the product of the
     % corresponding dimensional weights
-    weights(iPoint, :) = prod(localDimensionalWeights(index23));
+    weight(iPoint, :) = prod(localDimensionalWeights(index23));
+    % compute the index of each point in current cell. index is stored in
+    % columns
+    localIndexes = bsxfun(@pluss, indexOffset, localLowerIndex');
     index(iPoint, :) = 
 end
 
