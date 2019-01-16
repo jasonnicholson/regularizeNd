62b7023b01c52cd23557c741a576de1e8d50c7a3
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2019-01-15 20:57:06 -0600

    Had to convert all examples and documentation to mlx files.
    Signed-off-by: Jason Nicholson <1058191+jasonnicholson@users.noreply.github.com>

--- Examples/From gridfit/readme.m
+++
@@ -1,5 +0,0 @@
-% This examples were originally a part of gridfit. gridfit can find on the
-% Mathworks file exchange. This examples have been adapted to use
-% regularizeNd. If you are familar with gridfit, then this gives you a
-% reference for comparison. Otherwise, these examples just give you more
-% examples to understand regularizeNd.
