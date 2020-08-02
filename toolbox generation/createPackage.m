6a622036fb16fda8a71a61d510a7d911abd8ee8c
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2020-08-02 14:06:42 -0500

    - Added HTML for the examples. - Updated createPackage.m to check the HTML files. - Added template demos.xml.
    Signed-off-by: Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>

--- toolbox generation/createPackage.m
+++ toolbox generation/createPackage.m
@@ -23,6 +23,9 @@ mkdir(documentationFolder);
 % Copy getting started live script to doc folder
 copyfile("..\doc\GettingStarted.mlx", documentationFolder);
 
+% check that documentation is up to date
+checkHtmlFilesAreUpToDate("..\doc", "publish");
+
 % copy published documentation pages and helptoc.xml
 copyfile("..\doc\html", documentationFolder);
 
@@ -38,8 +41,74 @@ catch exception
 end
 
 %% Setup Examples
+% check that the examples are up to date
+checkHtmlFilesAreUpToDate("..\Examples", "error");
+
 copyfile("..\Examples\", BUILD_FOLDER_NAME + "\Examples");
 
 %% Package Toolbox
 % PROJECT_FILE = "regularizeNd.prj";
 % matlab.addons.toolbox.packageToolbox(PROJECT_FILE)
+
+% cannot manually package the toolbox
+beep;
+fprintf("Open the package toolbox with the prj file. Then modify the demos.xml file manually. Use demos.xml template as a starting point.\n");
+
+function checkHtmlFilesAreUpToDate(sourceFileDirectory, whatToDoFlag)
+% checkHtmlFilesAreUpToDate checks that html files exist and are up to date that were generated from mlx files
+
+
+sourceFiles = struct2table(dir(fullfile(sourceFileDirectory, "**/*.mlx")));
+
+for iFile = 1:height(sourceFiles)
+    fileNameWithExtension = sourceFiles.name{iFile};
+    [~,fileName,~] = fileparts(fileNameWithExtension);
+    
+    % skip readme files
+    if strcmpi(fileName,"Readme")
+        continue;
+    end
+    
+    % check that html folder exists
+    htmlFolder = fullfile(sourceFiles.folder{iFile},"html");
+    if exist( htmlFolder, "dir")
+        
+        % check for an html file
+        htmlFile = fullfile(htmlFolder,fileName + ".html");
+        htmlFileAttributes = dir(htmlFile);
+        if isempty(htmlFileAttributes)
+            publishLiveScript(sourceFiles.folder{iFile}, fileNameWithExtension, whatToDoFlag);
+        else
+            % check that the html file is always newer than the source file
+            if htmlFileAttributes.datenum < sourceFiles.datenum(iFile)
+                publishLiveScript(sourceFiles.folder{iFile}, fileNameWithExtension, whatToDoFlag);
+            end
+        end
+    else
+        % create the html folder and publish the file
+        mkdir(fullfile(sourceFiles.folder{iFile}, "html"));
+        publishLiveScript(sourceFiles.folder{iFile},fileNameWithExtension, whatToDoFlag);
+    end
+end
+
+end
+
+function publishLiveScript(folder,file, whatToDoFlag)
+
+switch lower(whatToDoFlag)
+    case "publish"
+        htmlFolder = fullfile(folder,"html");
+        try
+            addpath(folder);
+            publish(fullfile(folder,file),"outputDir", htmlFolder);
+            rmpath(folder);
+        catch e
+            rmpath(folder);
+            rethrow(e);
+        end
+    case "error"
+        error("createPackage:HTMLisOutOfDate", "%s is out of date or missing.", fullfile(folder,file));
+    otherwise
+        error("createPackage:badSwitchStatement", "Something is wrong with the switch statement. Code should not reach this.");
+end
+end
