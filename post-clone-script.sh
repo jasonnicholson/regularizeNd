50e4039bb17d2d7988a8a46f4e759233cf6cb45e
Jason H Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2023-03-29 21:30:15 -0500

    chore: Setting up git-conventional-commits

---
+++ post-clone-script.sh
@@ -0,0 +1,6 @@
+# includes the repo .gitconfig with the local .git/config
+'git config --local include.path ../.gitconfig' 
+
+# Install nodejs which comes with npm. I use NVM. I get it via winget:
+# winget install --id CoreyButler.NVMforWindows
+npm ci
