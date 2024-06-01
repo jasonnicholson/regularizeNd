66c4cdeb284573a2ab2323b64b06ddac6b5b5423
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2024-05-31 22:00:54 -0400

    chore: Updated dev depencies and post clone script

--- post-clone-script.sh
+++ post-clone-script.sh
@@ -1,5 +1,5 @@
 # includes the repo .gitconfig with the local .git/config
-'git config --local include.path ../.gitconfig' 
+git config --local include.path ../.gitconfig
 
 # Install nodejs which comes with npm. I use NVM. I get it via winget:
 # winget install --id CoreyButler.NVMforWindows
