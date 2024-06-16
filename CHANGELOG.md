d02f46ebcb057539a048328ed88d6e1b434525ee
Jason H. Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2024-06-15 20:40:36 -0400

    chore: updating packaging
    html files needed updated and corresponding mlx
    createPackage.m needed updated to work on Linux

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,24 @@
+## **3.0.0**&emsp;<sub><sup>2024-06-15 ([2c7bbe7...d7c4b71](https://github.com/jasonnicholson/regularizeNd/compare/2c7bbe725a35b8902766c55cad2df02a904ba01b...d7c4b7171d58d237ca29dd8f6885a77513c2dca6?diff=split))</sup></sub>
+
+### Documentation
+
+- Update examples related to new regularizeNd interface ([061c25c](https://github.com/jasonnicholson/regularizeNd/commit/061c25cffc95bee90462859636c8ddbd24ef4890))
+- updating documentation after interface update ([e6442d3](https://github.com/jasonnicholson/regularizeNd/commit/e6442d36f5cfaab6595e84efb5a2db896eeeea93))
+
+### Features
+
+- updating regularizeNd based on MATLAB language changes, targeting 2019b or newer ([4b516c5](https://github.com/jasonnicholson/regularizeNd/commit/4b516c5c4488fb24b940d2500e982fc3f5c20b28))
+
+### Bug Fixes
+
+- working on adding unit tests ([922e379](https://github.com/jasonnicholson/regularizeNd/commit/922e3794e29c6f6283ee52a286be8910127cd4a3))
+- reorganized regularizeNd to use regularizeNdMatrices ([25159fe](https://github.com/jasonnicholson/regularizeNd/commit/25159fee740932e9e170e115f83bbc2acbd2bc55))
+
+
+### BREAKING CHANGES
+-  updating regularizeNd based on MATLAB language changes, targeting 2019b or newer ([4b516c5](https://github.com/jasonnicholson/regularizeNd/commit/4b516c5c4488fb24b940d2500e982fc3f5c20b28))
+<br>
+
 ## **2.4.1**&emsp;<sub><sup>2023-04-05 ([50e4039...2f225a1](https://github.com/jasonnicholson/regularizeNd/compare/50e4039bb17d2d7988a8a46f4e759233cf6cb45e...2f225a105a9ba1d2b25397070be188e9326dfb43?diff=split))</sup></sub>
 
 ### Documentation
--- toolbox generation/createPackage.m
+++ toolbox generation/createPackage.m
@@ -2,7 +2,7 @@ clc; clear; close all;
 
 %% Setup directory
 % build folder is always on level up in the folder structure
-BUILD_FOLDER_NAME = "..\build";
+BUILD_FOLDER_NAME = fullfile("..", "build");
 
 % clean up previous builds
 if exist(BUILD_FOLDER_NAME,'dir')
@@ -14,23 +14,23 @@ mkdir(BUILD_FOLDER_NAME);
 
 %% Setup top level of toolbox
 copyfile("info.xml", BUILD_FOLDER_NAME);
-copyfile('..\source', BUILD_FOLDER_NAME);
+copyfile(fullfile("..", "source"), BUILD_FOLDER_NAME);
 
 %% Setup documentation
-documentationFolder = BUILD_FOLDER_NAME + "\doc";
+documentationFolder = fullfile(BUILD_FOLDER_NAME, "doc");
 mkdir(documentationFolder);
 
 % Copy Live script documentation to documentation folder
-copyfile("..\doc\*.mlx", documentationFolder);
+copyfile(fullfile("..", "doc", "*.mlx"), documentationFolder);
 
 % check that documentation is up to date
-checkHtmlFilesAreUpToDate("..\doc", "publish");
+checkHtmlFilesAreUpToDate(fullfile("..", "doc"), "publish");
 
 % copy published documentation pages and helptoc.xml
-copyfile("..\doc\html", documentationFolder);
+copyfile(fullfile("..", "doc", "html"), documentationFolder);
 
 % copy the changelog
-copyfile("..\CHANGELOG.md",BUILD_FOLDER_NAME);
+copyfile(fullfile("..", "CHANGELOG.md"),BUILD_FOLDER_NAME);
 
 % build the documentation search database
 % use try-catch to make sure path is restored
@@ -45,15 +45,15 @@ end
 
 %% Setup Examples
 % check that the examples are up to date
-checkHtmlFilesAreUpToDate("..\Examples", "error");
+checkHtmlFilesAreUpToDate(fullfile("..", "Examples"), "error");
 
-copyfile("..\Examples\", BUILD_FOLDER_NAME + "\Examples");
+copyfile(fullfile("..", "Examples"), fullfile(BUILD_FOLDER_NAME, "Examples"));
 
 %% Package Toolbox
 % PROJECT_FILE = "regularizeNd.prj";
 % matlab.addons.toolbox.packageToolbox(PROJECT_FILE)
 
-% cannot manually package the toolbox
+% cannot automatically package the toolbox
 beep;
 fprintf("Open the package toolbox with the prj file. Then modify the demos.xml file manually. Use demos.xml template as a starting point.\n");
 
@@ -62,7 +62,7 @@ function checkHtmlFilesAreUpToDate(sourceFileDirectory, whatToDoFlag)
 % checkHtmlFilesAreUpToDate checks that html files exist and are up to date that were generated from mlx files
 
 
-sourceFiles = struct2table(dir(fullfile(sourceFileDirectory, "**/*.mlx")));
+sourceFiles = struct2table(dir(fullfile(sourceFileDirectory, "**", "*.mlx")));
 
 for iFile = 1:height(sourceFiles)
     fileNameWithExtension = sourceFiles.name{iFile};
@@ -101,10 +101,11 @@ function publishLiveScript(folder,file, whatToDoFlag)
 
 switch lower(whatToDoFlag)
     case "publish"
-        htmlFolder = fullfile(folder,"html");
+        [~,baseName] = fileparts(file);
+        htmlFile = fullfile(folder,"html",baseName + ".html");
         try
             addpath(folder);
-            publish(fullfile(folder,file),"outputDir", htmlFolder);
+            export(fullfile(folder,file), htmlFile);
             rmpath(folder);
         catch e
             rmpath(folder);
